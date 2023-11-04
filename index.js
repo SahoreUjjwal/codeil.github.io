e1dd54cfa3a4427ed8840e06c6c4cbc8d9b42a1f
SahoreUjjwal <ujjwalsahorework@gmail.com>
2023-11-05 03:32:53 +0530

    added sign-up

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const express = require('express');
-
+const cookieParser = require('cookie-parser');
 
 const app = express();
 const port = 8000;
 
+const expressLayouts = require('express-ejs-layouts');
+
+const db = require('./config/mongoose.js');
+//extract styles & scripts used to extract custom scripts/styles from partial views into layout
+
+app.set('layout extractStyles',true);
+app.set('layout extractScripts',true);
+
 //require routes using middleware
+app.use(expressLayouts);
+
+app.use(cookieParser());
+
+app.use(express.static('./assets'));
+
+app.use(express.urlencoded());
+
 app.use('/',require('./routes'));
 
 app.use('/user',require('./routes/user'));
--- routes/user.js
+++ routes/user.js
@@ -5,6 +5,9 @@ const profileController = require('../controllers/profile_controller');
 
 router.get('/profile',profileController.profile);
 router.get('/name',profileController.name);
+router.get('/sign-in',profileController.signin);
+router.get('/sign-up',profileController.signup);
+router.post('/create',profileController.create);
 
 
 module.exports = router;
